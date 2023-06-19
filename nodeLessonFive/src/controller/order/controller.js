ae41f628876a5a8f70dc828d433844324385bbac
hoangdv <dinhvuhoang999@gmail.com>
2023-06-19 17:59:49 +0700

    bt-5

--- nodeLessonFive/src/controller/order/controller.js
+++ nodeLessonFive/src/controller/order/controller.js
@@ -3,41 +3,59 @@ const { Cart, User, Product, Order } = require('../../model');
 module.exports = {
   payForOrder: async (req, res, next) => {
     try {
-      const { productId } = req.body;
+      const { productListId, UserId } = req.body;
 
-      const product = await Product.findById(productId);
-      const order = await Order.findOne({})
-
-      if (!product) {
-        return res.status(404).json({
-          message: 'Product not found'
-        });
+      for (const item of productListId) {
+        const product = await Product.findById(item.productId);
+        if (!product) {
+          return res.status(404).json({
+            message: 'Product not found'
+          });
+        }
       }
+
       const cart = await Cart.findOne({});
-      const findProduct = cart.cartInfo.findIndex((cart) => {
-        return productId === cart.productId
-      })
+      const dataListProduct = [];
+      let price = 0;
 
-      if (findProduct === -1) {
-        return res.status(404).json({
-          message: 'Product not found in cart'
-        });
+      for (const item of cart.cartInfo) {
+        for (let i = 0; i < productListId.length; i += 1) {
+          const productSingle = await Product.findById(item.productId)
+          if (productSingle.quantity < item.quantity) {
+            return res.status(404).json({
+              message: 'some product in cart is change'
+            });
+          }
+
+          if (productListId[i].productId === item.productId) {
+            dataListProduct.push({
+              productListId: productSingle._id
+            });
+            price += productSingle.price
+            break;
+          }
+        }
       }
 
-      const findProductInOrder = order.products.findIndex((productItem) => {
-        console.log(productItem);
-        return productId === productItem.productId
+
+      for (const item of cart.cartInfo) {
+
+        const findProduct = cart.cartInfo.findIndex((cart) => {
+          console.log(cart);
+          console.log(item.productId);
+          return item.productId === cart.productId
+        })
+        cart.cartInfo.splice(findProduct, 1); //delete cart when add order 
+        console.log(findProduct);
+      }
+
+      await Order.create({
+        userId: UserId,
+        products: dataListProduct,
+        totalPrice: price
       });
 
-      if (findProductInOrder === -1) {
-        order.products.push({
-          productId
-        })
-        await order.save();
-      } else {
-        order.totalPrice = order.totalPrice + product.price
-        await order.save();
-      }
+      await cart.save();
 
       return res.json({ messenger: "Add success order" })
     } catch (error) {
